656a7a1581ed22771167ee24cc645e6eb5b14836
Leon Gebre <gelf.learing@gmail.com>
2024-04-24 22:10:38 +0300

    finding a better way for adding bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,22 +1,16 @@
 -- script that creates a stored procedure AddBonus that adds a new correction for a student
 DELIMITER $$
-CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
+CREATE PROCEDURE AddBonus (
+    IN user_id int,
+    IN project_name varchar(255),
+    IN score float
+)
 BEGIN
-    CREATE TABLE IF NOT EXISTS projects (
-        id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        PRIMARY KEY (id),
-        UNIQUE KEY (name)
-    );
+    INSERT INTO projects (name)
+    SELECT project_name FROM DUAL
 
-    INSERT INTO projects (name) VALUES (project_name)
-    ON DUPLICATE KEY UPDATE name = name;
-
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES (
-        user_id,
-        (SELECT id FROM projects WHERE name = project_name),
-        score
-    );
-END$$
-DELIMITER ;
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END;
+$
