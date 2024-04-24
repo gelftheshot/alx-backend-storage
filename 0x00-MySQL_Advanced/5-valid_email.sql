9234ce7323197149d0d839f6a132dcd084000c1a
Leon Gebre <gelf.learing@gmail.com>
2024-04-24 17:24:30 +0000

    reseting the vale of valid email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = CASE WHEN NEW.valid_email = 1 THEN 0 ELSE 1 END;
+        SET NEW.valid_email = CASE WHEN NEW.valid_email = 0;
     END IF;
 END //
 
