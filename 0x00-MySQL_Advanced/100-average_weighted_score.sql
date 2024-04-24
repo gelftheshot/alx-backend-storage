eea4b3d0479c5e1529a3fa04d0d3580fbf57a57c
root <root@joys>
2024-04-24 21:52:35 +0300

    weighted avrage of value

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,15 @@
+-- weighted avrage of user
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+BEGIN
+    DECLARE w_avg_score FLOAT;
+    SET w_avg = (SELECT SUM(score * weight) / SUM(weight)
+                        FROM users AS U
+                        JOIN corrections as C ON U.id=C.user_id
+                        JOIN projects AS P ON C.project_id=P.id
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = w_avg WHERE id=user_id;
+END
+$$
+DELIMITER ;
