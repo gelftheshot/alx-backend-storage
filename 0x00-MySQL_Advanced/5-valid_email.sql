c962bb704942c6a6f20c2a73760696cae1e4b2e7
Leon Gebre <gelf.learing@gmail.com>
2024-04-24 13:40:30 +0000

    changing the vaid email when email is change

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+DELIMITER //
+
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = CASE WHEN NEW.valid_email = 1 THEN 0 ELSE 1 END;
+    END IF;
+END //
+
+DELIMITER ;
