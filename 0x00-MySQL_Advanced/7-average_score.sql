33b959207af438b1aaeb15b8a16c5ee8a8b1d657
Leon Gebre <gelf.learing@gmail.com>
2024-04-24 17:49:43 +0000

    avrange of scor to a user

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,8 @@
+-- avrage of the score of each projects
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id FLOAT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id) WHERE id = user_id;
+END$$
+DELIMITER ;
