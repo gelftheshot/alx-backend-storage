5431dcceef2dbdd0bb8a201a7c9ffafa9e777f97
root <root@joys>
2024-04-24 21:44:56 +0300

    fixing syntax error nad adding view

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,15 +1,15 @@
 -- function for divition and reutn the float value
 DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(a int, b int)
-RETURN FLOAT
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
 BEGIN
-	DECLARE result FLOAT;
-	IF b = 0 THEN
-		SET result = 0;
-	ELSE
-		SET result = a / b;
-	END IF;
-	RETURN result;
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
 END;
 $$
---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- creating view for something important
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting
+AS SELECT name FROM students
+WHERE score < 80 AND last_meeting is NULL
+OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
