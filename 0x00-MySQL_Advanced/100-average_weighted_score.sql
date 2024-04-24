bd9a709be16ca8471c8720b74cb9b96b7b9563f6
Leon Gebre <gelf.learing@gmail.com>
2024-04-24 21:57:11 +0300

    updating syntax problem perhabs

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
     DECLARE w_avg_score FLOAT;
-    SET w_avg = (SELECT SUM(score * weight) / SUM(weight)
+    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
                         FROM users AS U
                         JOIN corrections as C ON U.id=C.user_id
                         JOIN projects AS P ON C.project_id=P.id
                         WHERE U.id=user_id);
-    UPDATE users SET average_score = w_avg WHERE id=user_id;
+    UPDATE users SET average_score = w_avg_score WHERE id=user_id;
 END
 $$
 DELIMITER ;
--- 0x00-MySQL_Advanced/README.md
+++ 0x00-MySQL_Advanced/README.md
@@ -1,2 +1,3 @@
 this is the readme file for 0x00-MySQL_Advanced
 the updated readme
+the updated readme
