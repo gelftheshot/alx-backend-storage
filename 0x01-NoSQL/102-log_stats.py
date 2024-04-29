398564a29bdda3272b08377ea8ecca0f140fd0cc
Leon Gebre <gelf.learing@gmail.com>
2024-04-29 19:40:40 +0300

    pycodestyle update

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python3
-""" write something about somekind of nginx log file """
+"""Script to analyze nginx log file."""
 from pymongo import MongoClient
-
-
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_coll = client.logs.nginx
-    print(nginx_coll.count_documents({}), "logs")
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print(f"{nginx_coll.count_documents({})} logs")
     print("Methods:")
-    print("\tmethod GET: {}".format(nginx_coll.count_documents({'method': 'GET'})))
-    print("\tmethod POST: {}".format(nginx_coll.count_documents({'method': 'POST'})))
-    print("\tmethod PUT: {}".format(nginx_coll.count_documents({'method': 'PUT'})))
-    print("\tmethod PATCH: {}".format(nginx_coll.count_documents({'method': 'PATCH'})))
-    print("\tmethod DELETE: {}".format(nginx_coll.count_documents({'method': 'DELETE'})))
-    print("{} status check".format(nginx_coll.count_documents({'method': 'GET', 'path': '/status'})))
+
+    for method in methods:
+        count = nginx_coll.count_documents({'method': method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = nginx_coll.count_documents(
+        {'method': 'GET', 'path': '/status'}
+    )
+    print(f"{status_check} status check")
+
     pipeline = [
-    {
-        '$group': {
-            '_id': '$ip',
-            'count': { '$sum': 1 }
-        }
-    },
-    {
-        '$sort': {
-            'count': -1
-        }
-    },
-    {
-        '$limit': 10
-    }
+        {'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}},
+        {'$limit': 10}
     ]
-    final = nginx_coll.aggregate(pipeline)
+
     print("IPs:")
-    for doc in final:
+    for doc in nginx_coll.aggregate(pipeline):
         print(f"\t{doc['_id']} {doc['count']}")
