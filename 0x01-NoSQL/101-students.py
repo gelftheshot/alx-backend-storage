a8ef2f88136989b3907d290743a070be5930c724
Leon Gebre <gelf.learing@gmail.com>
2024-04-29 18:21:29 +0300

    calculating avrange and updating it to the database

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function that returns all students sorted by average score """
+import pymongo
+
+
+def top_students(mongo_collection):
+    for doc in mongo_collection.find():
+        total = 0
+        leng = len(doc['topics'])
+        for sub in doc['topics']:
+            total += sub['score']
+        avr = total / leng
+        mongo_collection.update_one({'_id': doc['_id'] }, {'$set': {'averageScore': avr}})
+    return mongo_collection.find().sort('averageScore', pymongo.DESCENDING)
